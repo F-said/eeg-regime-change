67e6f8f7c7abfb3b6c9cdcfc57205ec64cc2a642
lk1422 <lk3ond@gmail.com>
2024-04-25 12:31:13 -0400

    final commit

--- CPD/test.py
+++ CPD/test.py
@@ -1,43 +1,54 @@
 from CPD import *
 import random
 import sys
+import matplotlib.pyplot as plt
 import numpy as np
 
 
+def plot_single_channel(data):
+    T = [i for i in range(data.shape[1])]
+    fig,axs = plt.subplots(data.shape[0], 1, sharex=True)
+    fig.set_size_inches(30.5, 32.5)
+    axs.plot(T, data[0])
+    plt.show()
 
-"""
-test_data = [1] * 100 + [2]*100 + [3]*100
-test_data = [test_data[i] + random.random()/8 for i in range(len(test_data))]
-test_data = np.array(test_data).reshape(1, -1)
-alg = Offline(3, model="gauss", p=2)
-#alg = basicHMM(3, covariance='full')
-out = alg.find_change_points_bin(eeg_data)
-print(out)
-#print(alg.find_change_points_opt(eeg_data))
-#print(alg.find_change_points_bin(eeg_data))
-"""
+def plot_tsa(data):
+    T = [i for i in range(data.shape[1])]
+    fig,axs = plt.subplots(data.shape[0], 1, sharex=True)
+    fig.set_size_inches(30.5, 32.5)
+    for c in range(data.shape[0]):
+        axs[c].plot(T, data[c])
+    plt.show()
 
-if __name__ == "__main__":
+
+def main():
+    freq = sys.argv[2]
     dset = sys.argv[1]
-    approx = sys.argv[2]
-    regimes = int(sys.argv[3])
-    freq = int(sys.argv[4])
-    model = sys.argv[5] #Gauss, AR
-    if model == "AR":
-        p = int(sys.argv[6])
-    data = np.genfromtxt(dset, delimiter=" ").astype(np.float32)
+    data = np.genfromtxt(dset, delimiter="\t").astype(np.float32)#.reshape(1, -1)
+    print(data.shape)
+
+    if freq == 'plot_s':
+        plot_single_channel(data)
+
+    if freq == 'plot':
+        plot_tsa(data)
+
+    freq = int(freq)
+
+
     print(data.shape)
     data = np.array([data[:, i*freq] for i in range(data.shape[1]//freq)]).T
     print(data.shape)
-
-    if model == "AR":
-        alg = Offline(3, model=model, p=p)
-    else: 
-        alg = Offline(3, model=model)
-
-    if approx == "True":
-        print(alg.find_change_points_bin(data))
-    else: 
-        print(alg.find_change_points_opt(data))
+    model = OfflineWrapper(3, model='gauss', p=100)
+    cps = model.find_change_points_bin(data)
+    for cp in cps:
+        print([item*freq for item in cp])
+    if (len(cps) > 1):
+        changes = model.get_cp_k_votes(cps,3)
+        print("FINAL")
+        print([item*freq for item in changes])
 
 
+
+if __name__ == "__main__":
+    main()
